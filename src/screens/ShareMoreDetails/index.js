583c05a6dd155928aea2dfb12aaba4934080436b
headit74 <headit74@hotmail.com>
2020-08-26 01:04:34 -0400

    ShareMoreDetails Screen, DropDownPicker added

--- src/screens/ShareMoreDetails/index.js
+++ src/screens/ShareMoreDetails/index.js
@@ -13,11 +13,35 @@ import IconButton from '@/components/Button/IconButton';
 import Space from '@/components/Space';
 import Images from '@/styles/Images';
 import {scale} from '@/styles/Sizes';
+import DropDownPicker from 'react-native-dropdown-picker';
+import Icon from 'react-native-vector-icons/Feather';
 
 const styles = StyleSheet.create({
   uploadButton: {
     justifyContent: 'center',
     padding: 25,
+  },
+  dropDownContainer: {
+    height: 60 * scale,
+    marginVertical: 10 * scale
+  },
+  dropDownItem: {
+    justifyContent: 'flex-start',
+  },
+  dropDown: {
+    backgroundColor: Colors.grey_light,
+    color: Colors.grey_dark,
+    borderWidth: 0,
+  },
+  dropDownBack: {
+    borderWidth: 0,
+    backgroundColor: Colors.grey_light,
+    color: Colors.grey_dark
+  },
+  dropDownLabel: {
+    backgroundColor: Colors.grey_light,
+    color: Colors.grey_dark,
+    fontSize: 16 * scale
   }
 });
 
@@ -33,9 +57,38 @@ const ShareModeDetails = (props) => {
       <Space height={20 * scale}/>
       <TransBlueButton onPress={vm.onPressChoose} caption={__('upload_your_photo')}/>
       <Space height={20}/>
-      <GreyInput placeholder={__('email_address') + ' or ' + __('phone_number')} value={vm.emailOrPhone}
-                 onChangeText={vm.setEmailOrPhone}/>
-      <GreyInput placeholder={__('password')} value={vm.password} onChangeText={vm.setPassword}/>
+      <View style={{width: '90%', ...(Platform.OS !== 'android' && {zIndex: 20})}}>
+        <DropDownPicker
+          items={[
+            {label: __('male'), value: 'male'},
+            {label: __('female'), value: 'female'}
+          ]}
+          style={styles.dropDownBack}
+          containerStyle={styles.dropDownContainer}
+          itemStyle={styles.dropDownItem}
+          dropDownStyle={styles.dropDown}
+          labelStyle={styles.dropDownLabel}
+          onChangeItem={item => vm.setGender(item.value)}
+          placeholder={__('select_gender')}
+        />
+      </View>
+      <View style={{width: '90%', ...(Platform.OS !== 'android' && {zIndex: 10})}}>
+        <DropDownPicker
+          items={[
+            {label: 'A', value: 'A'},
+            {label: 'B', value: 'B'},
+            {label: 'O', value: 'O'},
+            {label: 'AB', value: 'AB'}
+          ]}
+          style={styles.dropDownBack}
+          containerStyle={styles.dropDownContainer}
+          itemStyle={styles.dropDownItem}
+          dropDownStyle={styles.dropDown}
+          labelStyle={styles.dropDownLabel}
+          onChangeItem={item => vm.setGender(item.value)}
+          placeholder={__('select_blood_type')}
+        />
+      </View>
       <BlueButton onPress={vm.onPressSubmit} caption={__('submit')}/>
       <Space height={26}/>
     </BoardWithHeader>
